e55101d33d9a0b1e87f595174d12cee34aba271b
Thiago Pereira <pereira.tp.thiago@gmail.com>
2023-10-14 16:32:49 -0300

    aula 3-2: configurando miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de website',
+          amount: 'R$12.000',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          date: '14/10/2023',
+        },
+        {
+          id: 2,
+          title: 'Alugel',
+          amount: '- R$1.100',
+          type: 'withdraw',
+          category: 'Casa',
+          date: '13/10/2023',
+        },
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
